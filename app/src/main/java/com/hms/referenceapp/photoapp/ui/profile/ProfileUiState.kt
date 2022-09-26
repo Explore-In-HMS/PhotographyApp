059c295bdeae13b39eb25d7917074fd52cc1f916
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-26 14:01:18 +0300

    Change variable name

--- app/src/main/java/com/hms/referenceapp/photoapp/ui/profile/ProfileUiState.kt
+++ app/src/main/java/com/hms/referenceapp/photoapp/ui/profile/ProfileUiState.kt
@@ -4,14 +4,14 @@ import com.hms.referenceapp.photoapp.data.model.UserProfileUiModel
 
 data class ProfileUiState(
     val loading: Boolean,
-    val isUserSignOut: List<Boolean>,
+    val isUserSignedOut: List<Boolean>,
     val userProfile: UserProfileUiModel,
     val error: List<String>
 ) {
     companion object {
         fun initial() = ProfileUiState(
             loading = false,
-            isUserSignOut = emptyList(),
+            isUserSignedOut = emptyList(),
             userProfile = UserProfileUiModel(null, null),
             error = emptyList()
         )
